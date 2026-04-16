ee65ca2e67746e7cb7dd7163a7845964e529192f
bhuwadatharva <bhuwad.atharva@gmail.com>
2026-04-16 16:35:14 +0530

    change a cors policy

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="AI Career Counsellor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://career-counsellor-2-h3c8.onrender.com", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
