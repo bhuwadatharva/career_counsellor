1820a395ac55361e14e3a0cb279f472e1f86015a
bhuwadatharva <bhuwad.atharva@gmail.com>
2026-07-11 13:27:53 +0530

    fix: update CORS middleware to allow all origins

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="AI Career Counsellor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://career-counsellor-2-h3c8.onrender.com", "http://localhost:5173", "https://career-counsellor-1-g01v.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
