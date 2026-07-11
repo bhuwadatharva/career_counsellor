02905e2e910d10342be05d6ee913404c954c9820
bhuwadatharva <bhuwad.atharva@gmail.com>
2026-07-11 12:19:28 +0530

    fix: disable credentials in CORS middleware

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="AI Career Counsellor API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://career-counsellor-2-h3c8.onrender.com", "http://localhost:5173", "https://career-counsellor-1-g01v.onrender.com"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
